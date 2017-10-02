ec12b2bde41477e78d186be34c6d03c1a6ac083c
Seung Chi <seungyoonchi@gmail.com>
2017-10-02 11:25:14 -0700

    set up web solution

---
+++ gcVisionApi.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="gcVisionApi.Web.MvcApplication" Language="C#" %>
---
+++ gcVisionApi.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(gcVisionApi.Web.Startup))]
+namespace gcVisionApi.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
