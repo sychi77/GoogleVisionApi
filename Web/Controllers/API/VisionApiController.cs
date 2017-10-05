27d9a2fc9bcd1b4d126bb11597591f502a761e61
Seung Chi <seungyoonchi@gmail.com>
2017-10-05 13:57:23 -0700

    Set up API key get for api controller

---
+++ Web/Controllers/API/VisionApiController.cs
@@ -0,0 +1,32 @@
+﻿using gcVisionApi.Web.Models.Domain;
+using gcVisionApi.Web.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace gcVisionApi.Web.Controllers.API
+{
+    [RoutePrefix("api/vision")]
+    public class VisionApiController : ApiController
+    {
+        VisionService visionService = new VisionService();
+
+        [Route("key"), HttpGet]
+        public HttpResponseMessage GetApiKey(int id)
+        {
+            try
+            {
+                ApiKey response = new ApiKey();
+                response = visionService.SelectApiKey();
+                return Request.CreateResponse(HttpStatusCode.OK, response);
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+        }
+    }
+}
