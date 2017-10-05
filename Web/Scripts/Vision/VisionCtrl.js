72e9b14215119a6a5be5dc811cc4f98c7f90afc3
Seung Chi <seungyoonchi@gmail.com>
2017-10-05 14:39:27 -0700

    Set up Angular scripts and on pages

---
+++ Web/Scripts/Vision/VisionCtrl.js
@@ -0,0 +1,28 @@
+﻿//Index Controller
+(function () {
+    "use strict";
+    angular
+        .module('mainApp')
+        .controller('visionController', visionController);
+
+    visionController.$inject = ['$scope', 'visionService', '$window', 'toastr'];
+
+    function visionController($scope, visionService, $window, toastr) {
+
+        var vm = this;
+        vm.$scope = $scope;
+        vm.visionService = visionService;
+        vm.$onInit = _init;
+        vm.$window = $window;
+        vm.toastr = toastr;
+
+        function _init() {
+            return
+        }
+        function _error(err) {
+            return console.log(err)
+        }
+
+    }
+})();
+
---
+++ Web/Scripts/Vision/VisionService.js
@@ -0,0 +1,43 @@
+﻿//Vision Service
+(function () {
+    "use strict";
+    angular
+        .module('mainApp')
+        .service('visionService', visionService);
+    visionService.$inject = ['$http', '$q'];
+
+    function visionService($http, $q) {
+        return {
+            getKey: _getKey,
+            //getImageAnalysis: _getImageAnalysis
+        };
+        function _getKey() {
+            var settings = {
+                url: "/api/vision/key",
+                method: 'GET',
+                cache: false,
+                responseType: 'json',
+                withCredentials: true
+            };
+            return $http(settings)
+                .then(_complete, _fail);
+        }
+        //function _getImageAnalysis(key, data) {
+        //    var settings = {
+        //        url: /* Google API Call here */,
+        //        method: 'GET',
+        //        cache: false,
+        //        responseType: 'json',
+        //        withCredentials: true
+        //    };
+        //    return $http(settings)
+        //        .then(_complete, _fail);
+        //}
+        function _complete(data) {
+            return data;
+        }
+        function _fail(err) {
+            return $q.reject(err);
+        }
+    }
+})();
